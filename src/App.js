6b0e4ef3affb771a18b77efa260850dd4fbbee7d
ashton8504 <ashtonlittle8504@gmail.com>
2023-06-22 19:03:46 -0400

    update

--- src/App.js
+++ src/App.js
@@ -3,10 +3,28 @@
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="card">
+      <Avatar />
+      <div className="data">
+       <Intro />
+      </div>
     </div>
   );
+}
+
+function Avatar() {
+    return <img className="avatar" src="photoOfSelf.jpg" alt="Ashton Little"/>
+}
+
+function Intro() {
+    return (
+        <div>
+            <h1>Ashton Little</h1>
+            <p>
+                React Developer that is passionate about all things code. When I am not coding I enjoy cycling, guitar and all things Nintendo. 
+            </p>
+        </div>
+    )
 }
 
 export default App;
